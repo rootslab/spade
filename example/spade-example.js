e22da7237d68fdf6049cea8b82336002bfa759cb
rootslab <44gatti@gmail.com>
2014-05-19 02:13:05 +0200

    v 0.0.3. Stay Away! On branch master modified:   example/spade-example.js modified:   lib/spade.js modified:   package.json

--- example/spade-example.js
+++ example/spade-example.js
@@ -5,4 +5,22 @@
 var log = console.log
     , util = require( 'util' )
     , Spade = require( '../' )
+    , client = Spade()
+    , cback = function ( err, data, fn ) {
+        log( 'ex.: cback gets:', err, fn( data[ 0 ] ) );
+    }
+    , i = 0
     ;
+
+client.on( 'ready', function () {
+    while ( ++i <= 100 ) {
+        client.commands.lpush( 'list1', 'ambarabàcciccìcoccò', cback );
+    };
+    setTimeout( function () {
+        client.commands.lrange( 'list1', 1, 3, function ( err, data, fn ) {
+            log( fn( data ) );
+        } )
+    }, 4000 );
+} );
+
+client.connect();
