7e7eddf0696e55f2aa476781d5d8a5b17be537f9
rootslab <44gatti@gmail.com>
2014-05-20 22:18:36 +0200

    v0.0.5. Stay Away! On branch master new file:   example/transaction-discard-example.js new file:   example/transaction-exec-example.js new file:   example/transaction-nested-multi-example.js modified:   lib/spade.js modified:   package.json

---
+++ example/transaction-nested-multi-example.js
@@ -0,0 +1,40 @@
+/* 
+ * Spade MONITOR Example
+ */
+
+var log = console.log
+    , util = require( 'util' )
+    , Spade = require( '../' )
+    , client = Spade()
+    , cback = function ( err, data, fn ) {
+        log( 'ex.: cback gets:', err, fn( data[ 0 ] ) );
+    }
+    , i = 0
+    ;
+
+client.on( 'error', function ( ocmd ) {
+    log( 'error', ocmd );
+} );
+
+client.on( 'ready', function () {
+    client.commands.multi( function ( err, data, fn ) {
+        log( 'multi:', fn( data )  );
+        setTimeout( function () {
+            client.commands.ping( function ( err, data, fn ) {
+                log( 'ping:', fn( data ) );
+                setTimeout( function () {
+                    client.commands.multi( function ( err, data, fn ) {
+                        log( 'multi:', fn( data ) );
+                        setTimeout( function () {
+                            client.commands.exec( function ( err, data, fn ) {
+                                log( 'exec:', fn( data ) );
+                            } );
+                        }, 2000 );
+                    } );
+                }, 2000 );
+            } );
+        }, 2000 );
+    } );
+} );
+
+client.connect();
