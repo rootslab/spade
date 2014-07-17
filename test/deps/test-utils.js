2fdb15029e8adc6c96c8f33b7cea72a084d4ff6d
rootslab <44gatti@gmail.com>
2014-07-17 15:28:54 +0200

    v0.34.2. Better code.. On branch master modified:   lib/spade.js modified:   package.json modified:   test/deps/test-utils.js

--- test/deps/test-utils.js
+++ test/deps/test-utils.js
@@ -1,109 +1,110 @@
 module.exports = ( function () {
-var log = console.log
-    , util = require( 'util' )
-    , Bolgia = require( 'bolgia' )
-    , doString = Bolgia.doString
-    , improve = Bolgia.improve
-    , ooo = Bolgia.circles
-    , ostr = ooo.str
-    , oarr = ooo.arr
-    , inspect = util.inspect
-    , keys = Object.keys
-    , iopt = {
-        showHidden : false
-        , depth : 3
-        , colors : true
-        , customInspect : true 
-    }
-    , inspect = function ( arg, opt ) {
-        return util.inspect( arg, improve( opt, iopt ) );
-    }
-    , format = function ( ename, args ) {
 
-        if ( doString( ename ) !== ostr ) return;
-        if ( doString( args ) !== oarr ) return;
+    var log = console.log
+        , util = require( 'util' )
+        , Bolgia = require( 'bolgia' )
+        , doString = Bolgia.doString
+        , improve = Bolgia.improve
+        , ooo = Bolgia.circles
+        , ostr = ooo.str
+        , oarr = ooo.arr
+        , inspect = util.inspect
+        , keys = Object.keys
+        , iopt = {
+            showHidden : false
+            , depth : 3
+            , colors : true
+            , customInspect : true 
+        }
+        , inspect = function ( arg, opt ) {
+            return util.inspect( arg, improve( opt, iopt ) );
+        }
+        , format = function ( ename, args ) {
 
-        switch ( ename ) {
+            if ( doString( ename ) !== ostr ) return;
+            if ( doString( args ) !== oarr ) return;
 
-            // oerr, command object with error
-            case 'error':
-                return inspect( [ args[ 0 ].cmd, args[ 0 ].err ], iopt );
+            switch ( ename ) {
 
-            // script_list
-            case 'cacheinit':
-                return inspect( [ args[ 0 ].length ], iopt );
+                // oerr, command object with error
+                case 'error':
+                    return inspect( [ args[ 0 ].cmd, args[ 0 ].err ], iopt );
 
-            // script_name, error_msg
-            case 'scriptfailure':
-                return inspect( args, iopt );
+                // script_list
+                case 'cacheinit':
+                    return inspect( [ args[ 0 ].length ], iopt );
 
-            // lua_script_cache
-            case 'cacheready':
-                return inspect( keys( args[ 0 ].cache ), iopt );
+                // script_name, error_msg
+                case 'scriptfailure':
+                    return inspect( args, iopt );
 
-            // password, reply, address
-            case 'authorized':
-            case 'authfailed':
+                // lua_script_cache
+                case 'cacheready':
+                    return inspect( keys( args[ 0 ].cache ), iopt );
 
-            // db, reply, address
-            case 'dbselected':
-            case 'dbfailed':
+                // password, reply, address
+                case 'authorized':
+                case 'authfailed':
 
-            // script_name, data, txt
-            case 'cacheload':
-                return inspect( args.slice( 0, 2 ), iopt );
+                // db, reply, address
+                case 'dbselected':
+                case 'dbfailed':
 
-            // address
-            case 'connect':
-            case 'offline':
-            case 'lost':
-            case 'ready':
-                return inspect( [ args[ 0 ].host, args[ 0 ].port ], iopt );
+                // script_name, data, txt
+                case 'cacheload':
+                    return inspect( args.slice( 0, 2 ), iopt );
 
-            // timeout, address
-            case 'timeout':
-                return inspect( args[ 0 ], iopt ); 
+                // address
+                case 'connect':
+                case 'offline':
+                case 'lost':
+                case 'ready':
+                    return inspect( [ args[ 0 ].host, args[ 0 ].port ], iopt );
 
-            // attempt, ms, address
-            case 'attempt':
-                return inspect( args.slice( 0, 2 ).concat( [ [ args[ 2 ].host, args[ 2 ].port ] ] ), iopt );
+                // timeout, address
+                case 'timeout':
+                    return inspect( args[ 0 ], iopt ); 
 
-            // message, formatter
-            case 'monitor':
-                return inspect( args.slice( 0, 1 ), iopt );
+                // attempt, ms, address
+                case 'attempt':
+                    return inspect( args.slice( 0, 2 ).concat( [ [ args[ 2 ].host, args[ 2 ].port ] ] ), iopt );
 
-            // message
-            case 'message':
-                return inspect( args[ 0 ], iopt );
+                // message, formatter
+                case 'monitor':
+                    return inspect( args.slice( 0, 1 ), iopt );
 
-            // listen
-            case 'listen':
-                return inspect( args, iopt );
+                // message
+                case 'message':
+                    return inspect( args[ 0 ], iopt );
 
-            // shutup
-            case 'shutup':
-                return inspect( args, iopt );
+                // listen
+                case 'listen':
+                    return inspect( args, iopt );
 
-            case 'reply':
-                return inspect( [ args[ 0 ], args[ 1 ].cmd, args[ 2 ] ], iopt );
+                // shutup
+                case 'shutup':
+                    return inspect( args, iopt );
 
-            // object commands, queue_size
-            case 'queued':
-                return inspect( [ args[ 1 ], args[ 0 ].cmd, args[ 0 ].ecmd ], iopt );
+                case 'reply':
+                    return inspect( [ args[ 0 ], args[ 1 ].cmd, args[ 2 ] ], iopt );
 
-            // queue_size
-            case 'scanqueue':
-                return inspect( args, iopt );
+                // object commands, queue_size
+                case 'queued':
+                    return inspect( [ args[ 1 ], args[ 0 ].cmd, args[ 0 ].ecmd ], iopt );
 
-            default:
-                return inspect( args, iopt );
+                // queue_size
+                case 'scanqueue':
+                    return inspect( args, iopt );
+
+                default:
+                    return inspect( args, iopt );
+            };
+        }
+        ;
+
+        return {
+            format : format
+            , inspect : inspect
         };
-    }
-    ;
-
-    return {
-        format : format
-        , inspect : inspect
-    };
 
 } )();
