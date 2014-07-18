cfde5d418d5b65702fd9901e6c14ce875d4b38ee
rootslab <44gatti@gmail.com>
2014-07-18 02:54:47 +0200

    v0.35.2. Added test for mixes unsubscribe/punsubscribe commands. Update Syllabus to v0.31.5. On branch master

---
+++ test/pubsub-mixed-(p)unsubscribe-test.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+/* 
+ * Spade, pubsub mode events test.
+ */
+
+var debug = !! true
+    , emptyFn = function () {}
+    , log = console.log
+    , dbg = debug ? console.log : emptyFn
+    , assert = require( 'assert' )
+    , Bolgia = require( 'bolgia' )
+    , clone = Bolgia.clone
+    , test_utils = require( './deps/test-utils' )
+    , inspect = test_utils.inspect
+    , format = test_utils.format
+    , Spade = require( '../' )
+    , opt = {
+        security : {
+            '127.0.0.1:6379' : {
+                // disable db selection
+                db : -1
+            }
+        }
+    }
+    , client = Spade( clone( opt ) )
+    // expected events
+    , evts = []
+    // collected events
+    , eresult = []
+    ;
+
+log( '- created new Spade client with custom options:', inspect( opt ) );
+
+log( '- enable CLI logging.' );
+
+client.cli( true, function ( ename, args ) {
+    eresult.push( ename );
+    dbg( '  !%s %s', ename, format( ename, args || [] ) );
+} );
+
+log( '- opening client connection.' );
+
+client.connect( null, function () {
+
+    log( '- now client is connected and ready to send.' );
+    // push expected events
+    evts.push( 'connect', 'scanqueue', 'ready', 'listen' );
+
+    client.commands.subscribe( 'channel', function () {
+        // push expected event
+        evts.push( 'message' );
+        client.commands.psubscribe( 'chan*', function () {
+            // push expected event
+            evts.push( 'message' );
+            client.commands.unsubscribe( null, function () {
+                // push expected event
+                evts.push( 'message' );
+                client.commands.punsubscribe( null, function () {
+                    // push expected event
+                    evts.push( 'message' );
+                } );
+            } );
+        } );
+    } );
+
+} );
+
+
+log( '- now waiting 2 secs to collect events..' );
+
+setTimeout( function () {
+
+    var i = 0
+        ;
+
+    log( '- now disconnecting client with QUIT.' );
+
+    // push expected connection event
+    evts.push( 'reply', 'offline', 'lost' );
+
+    client.commands.quit( function ( is_err, reply, fn ) {
+        log( '- QUIT callback.', fn( reply ) );
+        assert.ok( fn( reply )[ 0 ] === 'OK' );
+        log( '- OK, client was disconnected.' );
+    } );
+
+    setTimeout( function () {
+        log( '- check collected events for client, should be:', inspect( evts ) );
+        assert.deepEqual( eresult, evts, 'got: ' + inspect( eresult ) );
+    }, 1000 );
+
+}, 2000 );
