8d84f044293eeb6faa2efcfa0d4c426ace6cd13c
rootslab <44gatti@gmail.com>
2014-07-22 05:35:37 +0200

    v0.39.0. Fixed bug with 'listen' with rollback subscriptions, reset parser and command queue on offline. Added subscriptions benchmarks for spade and node redis. Updated Hiboris to v1.2.1. Updated Libra to v0.16.4. On branch master renamed:    bench/node_redis-lrange-100-small-string-bench.js -> bench/node_redis-lrange-100-short-string-bench.js new file:   bench/node_redis-pubsub-long-message-bench.js new file:   bench/node_redis-pubsub-short-message-bench.js new file:   bench/run.sh renamed:    bench/spade-lrange-100-small-string-bench.js -> bench/spade-lrange-100-short-strig-bench.js new file:   bench/spade-pubsub-long-message-bench.js new file:   bench/spade-pubsub-short-message-bench.js modified:   lib/spade.js odified:   package.json

---
+++ bench/spade-pubsub-long-message-bench.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+/*
+ * NOTE:
+ * change default directive "client-output-buffer-limit pubsub" to "0 0 0" in redis.conf,
+ * if you need more requests, otherwise clients will be disconnected by Redis for security.
+ */
+
+var log = console.log
+    , assert = require( 'assert' )
+    , util = require( 'util' )
+    , inspect = util.inspect
+    , Spade = require( '../' )
+    // number of clients
+    , tclients = 20
+    // number of requests to send
+    , requests = 64 * 1024
+    // client list
+    , list = []
+    , channels = [ 'd', 'e', 'u', 'c', 'e', 's' ]
+    , rc = tclients * ( requests + channels.length )
+    , cc = tclients
+    , stime = 0
+    , ttime = 0
+    , long_string = 'spadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespadespade'
+    , spade_opt = { hiredis : !! true }
+    , publisher = null
+    , reply_messages = 0
+    ;
+
+var count = function () {
+   // log( - 1 + rc )
+    if ( --rc === 0 ) {
+        ttime = Date.now() - stime;
+        log( '-> subscribers (clients):', tclients );
+        log( '-> publishers:', 1 );
+        log( '-> total reply messages for client subscriptions:', channels.length * tclients );
+        assert.ok( reply_messages === channels.length * tclients, 'wrong number of messages replies form subscribe!' );
+        log( '-> total messages published:', requests );
+        log( '-> total messages received from publisher:', requests * tclients );
+        log( '-> total msecs:', ttime );
+        log( '-> rate:', Math.round( requests * tclients / ( ttime / 1000 ) ) + ' msgs/sec' );
+        process.exit( 0 );
+    }
+};
+
+var onError = function () {
+    log( 'error', arguments );
+    process.exit( 1 );
+};
+
+var sendCommands = function () {
+    var i = 0
+        , client = null
+        , pcmd = publisher.commands.publish
+        ;
+    stime = Date.now();
+    for ( ; i < requests; ++i ) pcmd( 'd', long_string );
+};
+
+var enqueue = function () {
+    var me = this
+        ;
+    me.commands.subscribe( channels, function () {
+        ++reply_messages;
+    } );
+    if ( --cc === 0 ) {
+        sendCommands();
+    }
+};
+
+var run = function () {
+    var i = 0
+        , n = tclients
+        , s = null
+        ;
+    for ( ; i < n; ++i ) {
+        s = Spade( spade_opt );
+        list[ i ] = s;
+        s.on( 'error', onError );
+        s.on( 'message', count );
+        // only once to prevent error on disconnection by Redis
+        s.once( 'ready', enqueue.bind( s ) );
+    };
+    for ( ; --i >= 0; ) {
+        list[ i ].connect();
+    };
+};
+
+var add = function () {
+    var s = Spade( spade_opt )
+        , commands = s.commands
+        ;
+    publisher = s;
+    s.once( 'ready', function () {
+        run();
+    } );
+    s.connect();
+};
+
+log( '\n> Spade benchmark, %d subscribers / 1 publisher', tclients );
+
+log( '\n-> using: "%s" parser.', Spade( spade_opt ).parser.hreader ? 'HIREDIS NATIVE' : 'BORIS JS' );
+log( '-> message length: (%d bytes)\n', long_string.length );
+// log( '"%s"\n', long_string );
+
+add();
---
+++ bench/spade-pubsub-short-message-bench.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+var log = console.log
+    , assert = require( 'assert' )
+    , Spade = require( '../' )
+    // number of clients
+    , tclients = 20
+    // number of requests to send
+    , requests = 64 * 1024
+    // client list
+    , list = []
+    , channels = [ 'd', 'e', 'u', 'c', 'e', 's' ]
+    , rc = tclients * ( requests + channels.length )
+    , cc = tclients
+    , stime = 0
+    , ttime = 0
+    , small_string = 'spadespadespadespadespadespadespadespadespadespadespadespade'
+    , spade_opt = { hiredis : !! true }
+    , publisher = null
+    , reply_messages = 0
+    ;
+
+var count = function () {
+    if ( --rc === 0 ) {
+        ttime = Date.now() - stime;
+        log( '-> subscribers (clients):', tclients );
+        log( '-> publishers:', 1 );
+        log( '-> total reply messages for client subscriptions:', channels.length * tclients );
+        assert.ok( reply_messages === channels.length * tclients, 'wrong number of messages replies form subscribe!' );
+        log( '-> total messages published:', requests );
+        log( '-> total messages received from publisher:', requests * tclients );
+        log( '-> total msecs:', ttime );
+        log( '-> rate:', Math.round( requests * tclients / ( ttime / 1000 ) ) + ' msgs/sec' );
+        process.exit( 0 );
+    }
+};
+
+var onError = function () {
+    log( 'error', arguments );
+    process.exit( 1 );
+};
+
+var sendCommands = function () {
+    var i = 0
+        , client = null
+        , pcmd = publisher.commands.publish
+        ;
+    stime = Date.now();
+    for ( ; i < requests; ++i ) pcmd( 'd', small_string );
+};
+
+var enqueue = function () {
+    var me = this
+        ;
+    me.commands.subscribe( channels, function () {
+        ++reply_messages;
+    } );
+    if ( --cc === 0 ) {
+        sendCommands();
+    }
+};
+
+var run = function () {
+    var i = 0
+        , n = tclients
+        , s = null
+        ;
+    for ( ; i < n; ++i ) {
+        s = Spade( spade_opt );
+        list[ i ] = s;
+        s.on( 'error', onError );
+        s.on( 'message', count );
+        s.on( 'ready', enqueue.bind( s ) );
+    };
+    for ( ; --i >= 0; ) {
+        list[ i ].connect();
+    };
+};
+
+var add = function () {
+    var s = Spade( spade_opt )
+        , commands = s.commands
+        ;
+    publisher = s;
+    s.once( 'ready', function () {
+        run();
+    } );
+    s.connect();
+};
+
+log( '\n> Spade benchmark, %d subscribers / 1 publisher', tclients );
+
+log( '\n-> using: "%s" parser.', Spade( spade_opt ).parser.hreader ? 'HIREDIS NATIVE' : 'BORIS JS' );
+log( '-> message length: (%d bytes)\n', small_string.length );
+// log( '"%s"\n', small_string );
+
+add();
